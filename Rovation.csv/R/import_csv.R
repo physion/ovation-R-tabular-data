7b4ea4570c70ded401533690a577e14ec00f0ff1
Barry Wark <barrywark@gmail.com>
2013-07-26 02:08:41 -0400

    We can use plyr's d_ply instead of a for() loop

--- Rovation.csv/R/import_csv.R
+++ Rovation.csv/R/import_csv.R
@@ -1,3 +1,4 @@
+library(plyr)
 
 #' Inserts Ovation Measurements for each row in a DataFrame
 #' 
@@ -40,35 +41,34 @@ ImportCSV <- function(context,
                                  List2Map(device.parameters))
   
   # Insert a measurement for each row
-  for(i in 1:nrow(df)) {
-  #measurements <- apply(df, 1, function(r) {
-    r <- df[i,]
-    source.label <- as.character(r[,source.label.column])
-    source.id <- as.character(r[,source.id.column])
-    source.name <- sprintf("%s-%s", source.label, source.id)
-    
-    cat(sprintf("  %s\n", source.name))
-    
-    sources <- as.list(context$getSources(source.label, source.id))
-    
-    if(length(sources) == 0) {
-      source <- context$insertSource(source.label, source.id)
-    } else if (length(sources) == 1) {
-      source <- sources[[1]]
-    } else {
-      simpleError(sprintf("Multiple Sources with label %s and ID %s exist in the database"))
-    }
-    
-    epoch$addInputSource(source.name, source)
-    
-    temp.file <- tempfile(fileext=".csv")
-    write.csv(r, file=temp.file, row.names=FALSE)
-    epoch$insertMeasurement(source.name,
-                            Vector2Set(c(source.name)),
-                            Vector2Set(c()),
-                            NewUrl(temp.file),
-                            "text/csv")
-  }
+  measurements <- d_ply(df, c(df[[source.label.column]], df[[source.id.column]]), 
+                        function(r) {
+                          source.label <- unique(as.character(r[[source.label.column]]))
+                          source.id <- unique(as.character(r[[source.id.column]]))
+                          source.name <- sprintf("%s-%s", source.label, source.id)
+                          
+                          cat(sprintf("  %s\n", source.name))
+                          
+                          sources <- as.list(context$getSources(source.label, source.id))
+                          
+                          if(length(sources) == 0) {
+                            source <- context$insertSource(source.label, source.id)
+                          } else if (length(sources) == 1) {
+                            source <- sources[[1]]
+                          } else {
+                            simpleError(sprintf("Multiple Sources with label %s and ID %s exist in the database"))
+                          }
+                          
+                          epoch$addInputSource(source.name, source)
+                          
+                          temp.file <- tempfile(fileext=".csv")
+                          write.csv(r, file=temp.file, row.names=FALSE)
+                          epoch$insertMeasurement(source.name,
+                                                  Vector2Set(c(source.name)),
+                                                  Vector2Set(c()),
+                                                  NewUrl(temp.file),
+                                                  "text/csv")
+                        })
   
   return(epoch)
 }
