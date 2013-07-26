8e7ce75e0fd6dcef18ea27f4911e78de84bbda32
Barry Wark <barrywark@gmail.com>
2013-07-26 01:46:30 -0400

    Imports CSV by rows
    Using a for() loop because I can't figure out how to break the
    dataframe to row-individual CSV in apply()… help please.

--- Rovation.csv/R/import_csv.R
+++ Rovation.csv/R/import_csv.R
@@ -4,14 +4,55 @@ ImportCSV <- function(context,
                       csv.path, 
                       start.time, 
                       end.time, 
-                      protocol, 
+                      protocol.uri, 
                       protocol.parameters, 
                       devices, 
                       device.parameters, 
-                      container,
-                      source.id.column = 1) {
+                      container.uri,
+                      source.label.column = 1,
+                      source.id.column = 2) {
   
   df <- read.csv(csv.path)
   
+  container <- context$getObjectWithURI(container.uri)
+  protocol <- context$getObjectWithURI(protocol.uri)
   
+  # Insert Epoch
+  cat("Inserting Epoch...\n")
+  epoch <- container$insertEpoch(start.time,
+                                 end.time,
+                                 protocol,
+                                 List2Map(protocol.parameters),
+                                 List2Map(device.parameters))
+  
+  # Insert a measurement for each row
+  for(i in 1:nrow(df)) {
+  #measurements <- apply(df, 1, function(r) {
+    r <- df[i,]
+    source.label <- as.character(r[,source.label.column])
+    source.id <- as.character(r[,source.id.column])
+    source.name <- sprintf("%s-%s", source.label, source.id)
+    
+    cat(sprintf("  %s\n", source.name))
+    
+    sources <- as.list(context$getSources(source.label, source.id))
+    
+    if(length(sources) == 0) {
+      source <- context$insertSource(source.label, source.id)
+    } else if (length(sources) == 1) {
+      source <- sources[[1]]
+    } else {
+      simpleError(sprintf("Multiple Sources with label %s and ID %s exist in the database"))
+    }
+    
+    epoch$addInputSource(source.name, source)
+    
+    temp.file <- tempfile(fileext=".csv")
+    write.csv(r, file=temp.file, row.names=FALSE)
+    epoch$insertMeasurement(source.name,
+                            Vector2Set(c(source.name)),
+                            Vector2Set(c()),
+                            NewUrl(temp.file),
+                            "text/csv")
+  }
 }
